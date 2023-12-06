250356b356fd5ef355a20413dc82bdc9a34b89a2
Josh Tully <jtully@impactnetworking.com>
2023-12-06 11:38:17 -0600

    update add in server side style sheet on document

--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,53 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
-    return (
-        <Html lang='en'>
-            <Head>
-                <script
-                    async
-                    src='https://www.googletagmanager.com/gtag/js?id=G-DEE33CNKP9'
-                />
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `window.dataLayer = window.dataLayer || [];
-                                function gtag(){dataLayer.push(arguments);}
-                                gtag('js', new Date());
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
 
-                                gtag('config', 'G-DEE33CNKP9');`,
-                    }}
-                />
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) =>
+                        sheet.collectStyles(<App {...props} />),
+                });
+
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: [initialProps.styles, sheet.getStyleElement()],
+            };
+        } finally {
+            sheet.seal();
+        }
+    }
+
+    render() {
+        return (
+            <Html lang='en'>
+                <Head>
+                    <script
+                        async
+                        src='https://www.googletagmanager.com/gtag/js?id=G-DEE33CNKP9'
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `window.dataLayer = window.dataLayer || [];
+                                    function gtag(){dataLayer.push(arguments);}
+                                    gtag('js', new Date());
+    
+                                    gtag('config', 'G-DEE33CNKP9');`,
+                        }}
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
+
+export default MyDocument;
